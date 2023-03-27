375e00b5929ee6b754b321f688d472698f786ac3
Luckyones <Luckyones@DESKTOP-Q2UTOEE>
2023-03-27 22:23:24 +0400

    Добавить README.md.

---
+++ Form2.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace SQLQueryEditor
+{
+    partial class MainWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menu = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.new_connection = new System.Windows.Forms.ToolStripMenuItem();
+            this.close_and_exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.visualizzatore_risultati = new System.Windows.Forms.ListView();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.query_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.esegui_query = new System.Windows.Forms.Button();
+            this.info_label = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menu.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.BackColor = System.Drawing.Color.LightGray;
+            this.menu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.menu.Controls.Add(this.label2);
+            this.menu.Controls.Add(this.menuStrip1);
+            this.menu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menu.Location = new System.Drawing.Point(-9, -5);
+            this.menu.Margin = new System.Windows.Forms.Padding(4);
+            this.menu.Name = "menu";
+            this.menu.Padding = new System.Windows.Forms.Padding(13, 10, 0, 0);
+            this.menu.Size = new System.Drawing.Size(1825, 53);
+            this.menu.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 29);
+            this.label2.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.LightGray;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(21, 10);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(81, 40);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.new_connection,
+            this.close_and_exit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(71, 36);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // new_connection
+            // 
+            this.new_connection.Name = "new_connection";
+            this.new_connection.Size = new System.Drawing.Size(321, 44);
+            this.new_connection.Text = "New connection";
+            this.new_connection.Click += new System.EventHandler(this.new_connection_Click);
+            // 
+            // close_and_exit
+            // 
+            this.close_and_exit.Name = "close_and_exit";
+            this.close_and_exit.Size = new System.Drawing.Size(321, 44);
+            this.close_and_exit.Text = "Close and exit";
+            this.close_and_exit.Click += new System.EventHandler(this.close_and_exit_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.visualizzatore_risultati, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(-1, 50);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1401, 814);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // visualizzatore_risultati
+            // 
+            this.visualizzatore_risultati.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.visualizzatore_risultati.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.visualizzatore_risultati.GridLines = true;
+            this.visualizzatore_risultati.HideSelection = false;
+            this.visualizzatore_risultati.Location = new System.Drawing.Point(36, 47);
+            this.visualizzatore_risultati.Margin = new System.Windows.Forms.Padding(4);
+            this.visualizzatore_risultati.Name = "visualizzatore_risultati";
+            this.visualizzatore_risultati.Size = new System.Drawing.Size(1328, 312);
+            this.visualizzatore_risultati.TabIndex = 0;
+            this.visualizzatore_risultati.UseCompatibleStateImageBehavior = false;
+            this.visualizzatore_risultati.View = System.Windows.Forms.View.Details;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.esegui_query, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.info_label, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 411);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1393, 398);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.query_box);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(4, 123);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1380, 160);
+            this.panel1.TabIndex = 0;
+            // 
+            // query_box
+            // 
+            this.query_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.query_box.Location = new System.Drawing.Point(213, 11);
+            this.query_box.Margin = new System.Windows.Forms.Padding(4);
+            this.query_box.Multiline = true;
+            this.query_box.Name = "query_box";
+            this.query_box.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.query_box.Size = new System.Drawing.Size(1115, 136);
+            this.query_box.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SQL Query:";
+            // 
+            // esegui_query
+            // 
+            this.esegui_query.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.esegui_query.Location = new System.Drawing.Point(617, 333);
+            this.esegui_query.Margin = new System.Windows.Forms.Padding(4);
+            this.esegui_query.Name = "esegui_query";
+            this.esegui_query.Size = new System.Drawing.Size(158, 49);
+            this.esegui_query.TabIndex = 1;
+            this.esegui_query.Text = "Execute";
+            this.esegui_query.UseVisualStyleBackColor = true;
+            this.esegui_query.Click += new System.EventHandler(this.exec_query_Click);
+            // 
+            // info_label
+            // 
+            this.info_label.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.info_label.AutoSize = true;
+            this.info_label.Location = new System.Drawing.Point(347, 47);
+            this.info_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.info_label.Name = "info_label";
+            this.info_label.Size = new System.Drawing.Size(698, 25);
+            this.info_label.TabIndex = 2;
+            this.info_label.Text = "Insert a query to execute. The result will be shown in the window above!";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(1505, 131);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(121, 97);
+            this.treeView1.TabIndex = 3;
+            // 
+            // MainWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1813, 863);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "MainWin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MySQLQueryEditor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWin_FormClosing);
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel menu;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListView visualizzatore_risultati;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox query_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button esegui_query;
+        private System.Windows.Forms.Label info_label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem new_connection;
+        private System.Windows.Forms.ToolStripMenuItem close_and_exit;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto.Tls;
+
+namespace SQLQueryEditor
+{
+    public partial class MainWin : Form
+    {
+        private DBManager db = DBManager.GetIstance();
+        public MainWin()
+        {
+
+            InitializeComponent();
+
+
+            treeView1.ExpandAll();
+            treeView1.Nodes.Add(db.GetDBname());
+            treeView1.Nodes[0].ExpandAll();
+            treeView1.Nodes[0].Nodes.Add("Table");
+        }
+
+
+        private void exec_query_Click(object sender, EventArgs e)
+        {
+            info_label.ForeColor = Color.Black;
+            info_label.Text = "Executing ...";
+            
+            Task.Run(() =>
+            {
+                try
+                {
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    Tuple<List<string>, List<string[]>> col_data = db.ExecuteQuery(query_box.Text);
+
+                   
+
+                    List<string> col = col_data.Item1;
+                    List<string[]> res = col_data.Item2;
+                    
+
+                    visualizzatore_risultati.Invoke((MethodInvoker)delegate
+                    {
+                        visualizzatore_risultati.Clear();
+
+                        foreach (string c in col)
+                            visualizzatore_risultati.Columns.Add(c);
+
+                        foreach (string[] r in res)
+                        {
+                            ListViewItem item = new ListViewItem(r);
+                            visualizzatore_risultati.Items.Add(item);
+                        }
+                        stopwatch.Stop();
+                    });
+
+
+                    info_label.Invoke((MethodInvoker)delegate
+                    {
+                        info_label.Text = "Query executed. Time " + Convert.ToDouble(stopwatch.ElapsedMilliseconds)  / 1000 + " s";
+                        info_label.ForeColor = Color.Black;
+                    });
+
+                }
+                catch (Exception exc)
+                {
+                    if (exc is MySqlException || exc is ArgumentNullException
+                        || exc is InvalidOperationException || exc is NullReferenceException)
+                    {
+                        info_label.Invoke((MethodInvoker) delegate
+                        {
+                            info_label.Text = exc.Message;
+                            info_label.ForeColor = Color.Red;
+                        });
+
+                    }
+                }
+            });
+        }
+
+        private void new_connection_Click(object sender, EventArgs e)
+        {
+            SQLQueryEditor conn = new SQLQueryEditor();
+            conn.Visible = true;
+        }
+
+        private void MainWin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            db.CloseConnection();
+        }
+
+        private void close_and_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
